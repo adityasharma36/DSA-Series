cc4cbb429b0903efcdde02fe90add684b8a3c0c3
adityasharma736 <adityasharma736630@gmail.com>
2025-07-03 17:36:15 +0530

    pointer class 2

---
+++ Pointers/class2.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a = 5;
+    // int *p = &a;
+    // int *q = p;
+
+    // // print value of a
+    // cout<<a<<endl;
+
+    // // print addres of value a
+    // cout<<&a<<endl;
+
+    // // print pointer a 
+    // // cout<<*a<<endl;
+
+    // // print value of p 
+    // cout<<p<<endl;
+
+    // // print address of p
+    // cout<<&p<<endl;
+
+    // // print value of address that store in p
+    // cout<<*p<<endl;
+
+    // // print value of address 
+    // cout<<q<<endl;
+
+    // // print address of q
+    // cout<<&q<<endl;
+
+    // // print value of address 
+    // cout<<*q<<endl;
+
+    // double pointer 
+    // cout<<**q<<endl;
+
+    int a = 10;
+
+    // pointer of integer a
+    int *p = &a;
+
+    // pointer to pointer of integer a
+    int **q = &p;
+
+    // print value of a
+    cout<<a<<endl;
+
+    // print address of a
+    cout<<&a<<endl;
+
+    // print pointer a
+    // cout<<*a<<endl;
+
+    // print value of p
+    cout<<p<<endl;
+
+    // print value of address
+    cout<<*p<<endl;
+
+    // print own address
+    cout<<&p<<endl;
+
+    // value of double pointer
+    // cout<<**p<<endl;
+
+    // print value q in pointer
+    cout<<q<<endl;
+
+    // print address of q
+    cout<<&q<<endl;
+
+    // print value of q
+    cout<<*q<<endl;
+
+    // print double pointer value 
+    cout<<**q<<endl;
+
+    // print thriple pointer value
+    // cout<<***q<<endl;
+}
